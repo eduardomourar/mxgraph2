295289a680d3b7cd33fd4860bba5e6aea1650e03
Gaudenz Alder <alderg@gmail.com>
2014-11-06 21:46:55 +0100

    Fixes perimeter of Call Conversation shape

--- javascript/examples/grapheditor/www/js/Sidebar.js
+++ javascript/examples/grapheditor/www/js/Sidebar.js
@@ -918,7 +918,7 @@ Sidebar.prototype.addBpmnPalette = function(dir, expand)
 		content.appendChild(this.createVertexTemplate('swimlane;html=1;horizontal=0;swimlaneFillColor=white;swimlaneLine=0;', 300, 120, 'Lane', 'Lane', true));
 		
 		content.appendChild(this.createVertexTemplate('shape=hexagon;html=1;perimeter=hexagonPerimeter;', 60, 50, '', 'Conversation', true));
-		content.appendChild(this.createVertexTemplate('shape=hexagon;html=1;strokeWidth=4', 60, 50, '', 'Call Conversation', true));
+		content.appendChild(this.createVertexTemplate('shape=hexagon;html=1;perimeter=hexagonPerimeter;strokeWidth=4', 60, 50, '', 'Call Conversation', true));
 		
 	    var classCell = new mxCell('', new mxGeometry(0, 0, 40, 30), 'shape=message;html=1;fillColor=#000000;strokeColor=#ffffff;strokeWidth=2;');
     	classCell.vertex = true;
