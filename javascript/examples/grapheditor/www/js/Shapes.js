115bff1f60e226295ad1ec8206413195528e6064
David Benson <github@jgpd.com>
2015-02-13 15:00:25 +0000

    Link shape now uses arrow shape paint

--- javascript/examples/grapheditor/www/js/Shapes.js
+++ javascript/examples/grapheditor/www/js/Shapes.js
@@ -905,46 +905,15 @@
 		mxArrow.call(this);
 	};
 	mxUtils.extend(LinkShape, mxArrow);
-	LinkShape.prototype.paintEdgeShape = function(c, pts)
+	
+	LinkShape.prototype.isOpenEnded = function()
 	{
-		var width = 10 + Math.max(0, this.strokewidth - 1) * 4;
+		return true;
+	};
 
-		// Base vector (between end points)
-		var p0 = pts[0];
-		var pe = pts[pts.length - 1];
-		
-		var dx = pe.x - p0.x;
-		var dy = pe.y - p0.y;
-		var dist = Math.sqrt(dx * dx + dy * dy);
-		var length = dist;
-		
-		// Computes the norm and the inverse norm
-		var nx = dx / dist;
-		var ny = dy / dist;
-		var basex = length * nx;
-		var basey = length * ny;
-		var floorx = width * ny/3;
-		var floory = -width * nx/3;
-		
-		// Computes points
-		var p0x = p0.x - floorx / 2;
-		var p0y = p0.y - floory / 2;
-		var p1x = p0x + floorx;
-		var p1y = p0y + floory;
-		var p2x = p1x + basex;
-		var p2y = p1y + basey;
-		var p3x = p2x + floorx;
-		var p3y = p2y + floory;
-		// p4 not necessary
-		var p5x = p3x - 3 * floorx;
-		var p5y = p3y - 3 * floory;
-		
-		c.begin();
-		c.moveTo(p1x, p1y);
-		c.lineTo(p2x, p2y);
-		c.moveTo(p5x + floorx, p5y + floory);
-		c.lineTo(p0x, p0y);
-		c.stroke();
+	LinkShape.prototype.getEdgeWidth = function()
+	{
+		return 4 + Math.max(0, this.strokewidth - 1) * 2;
 	};
 
 	// Registers the link shape
