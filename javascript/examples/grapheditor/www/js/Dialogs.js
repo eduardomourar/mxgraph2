be3516d7522ea80680ab8836e27f155353471058
Gaudenz Alder <alderg@gmail.com>
2015-07-15 10:22:24 +0200

    Takes footer into account for vertical dialog center

--- javascript/examples/grapheditor/www/js/Dialogs.js
+++ javascript/examples/grapheditor/www/js/Dialogs.js
@@ -19,7 +19,7 @@ function Dialog(editorUi, elt, w, h, modal, closable, onClose)
 	h += dx;
 	
 	var left = Math.max(0, Math.round((document.body.scrollWidth - w) / 2));
-	var top = Math.max(0, Math.round((Math.max(document.body.scrollHeight, document.documentElement.scrollHeight) - h) / 3));
+	var top = Math.max(0, Math.round((Math.max(document.body.scrollHeight, document.documentElement.scrollHeight) - h - editorUi.footerHeight) / 3));
 	
 	// Increments zIndex to put subdialogs and background over existing dialogs and background
 	if (editorUi.dialogs.length > 0)
