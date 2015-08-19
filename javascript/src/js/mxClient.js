b48333d3cf515993058934445b5e021cd59f7094
Gaudenz Alder <alderg@gmail.com>
2015-08-19 13:52:52 +0200

    Changes copyright notice

--- javascript/src/js/mxClient.js
+++ javascript/src/js/mxClient.js
@@ -1,9 +1,9 @@
 /**
- * Copyright (c) 2006-2013, JGraph Ltd
+ * Copyright (c) 2006-2015, JGraph Ltd
+ * Copyright (c) 2006-2015, Gaudenz Alder
  */
 var mxClient =
 {
-
 	/**
 	 * Class: mxClient
 	 *
