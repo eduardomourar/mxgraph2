e6738eb8b50de08c8baddca462f430363f59908d
Gaudenz Alder <alderg@gmail.com>
2015-09-16 11:02:59 +0200

    Adds hover style for virtual bends in mxEdgeHandler

--- javascript/src/js/handler/mxEdgeHandler.js
+++ javascript/src/js/handler/mxEdgeHandler.js
@@ -630,10 +630,23 @@ mxEdgeHandler.prototype.validateConnection = function(source, target)
 	{
 		for (var i = 1; i < this.abspoints.length; i++)
 		{
-			var bend = this.createHandleShape(i);
-			this.initBend(bend);
-			bend.setCursor(mxConstants.CURSOR_VIRTUAL_BEND_HANDLE);
-			bends.push(bend);
+			(mxUtils.bind(this, function(bend)
+			{
+				this.initBend(bend);
+				bend.setCursor(mxConstants.CURSOR_VIRTUAL_BEND_HANDLE);
+				
+				mxEvent.addListener(bend.node, 'mouseenter', mxUtils.bind(this, function()
+				{
+					mxUtils.setOpacity(bend.node, 100);
+				}));
+				
+				mxEvent.addListener(bend.node, 'mouseleave', mxUtils.bind(this, function()
+				{
+					mxUtils.setOpacity(bend.node, this.virtualBendOpacity);
+				}));
+				
+				bends.push(bend);
+			}))(this.createHandleShape(i));
 		}
 	}
 
