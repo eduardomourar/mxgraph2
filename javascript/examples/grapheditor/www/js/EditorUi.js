c249294bb7dc6ba1e995f202e9bcc01e3e1878b8
Gaudenz Alder <alderg@gmail.com>
2015-10-08 13:51:25 +0200

    Fixes Ctrl+B/U/I while editing in FF/Safari

--- javascript/examples/grapheditor/www/js/EditorUi.js
+++ javascript/examples/grapheditor/www/js/EditorUi.js
@@ -2733,10 +2733,11 @@ EditorUi.prototype.createKeyHandler = function(editor)
 	var isEventIgnored = keyHandler.isEventIgnored;
 	keyHandler.isEventIgnored = function(evt)
 	{
-		// Handles undo/redo via action and allows ctrl+b/u/i only if editing value is HTML (except for FF)
+		// Handles undo/redo via action and allows ctrl+b/u/i only if editing value is HTML (except for FF and Safari)
 		return (!this.isControlDown(evt) || mxEvent.isShiftDown(evt) || (evt.keyCode != 90 && evt.keyCode != 89)) &&
 			((evt.keyCode != 66 && evt.keyCode != 73 && evt.keyCode != 85) || !this.isControlDown(evt) ||
-			(this.graph.cellEditor.isContentEditing() && !mxClient.IS_FF)) && isEventIgnored.apply(this, arguments);
+			(this.graph.cellEditor.isContentEditing() && !mxClient.IS_FF && !mxClient.IS_SF)) &&
+			isEventIgnored.apply(this, arguments);
 	};
 	
 	// Routes command-key to control-key on Mac
