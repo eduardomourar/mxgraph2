384d9876db4b580783857f6ed4b1451cb7a9e86d
Gaudenz Alder <alderg@gmail.com>
2015-10-09 09:18:46 +0200

    Adds edit metadata to context menu

--- javascript/examples/grapheditor/www/js/Menus.js
+++ javascript/examples/grapheditor/www/js/Menus.js
@@ -1040,7 +1040,7 @@ Menus.prototype.createPopupMenu = function(menu, cell, evt)
 			if (graph.getSelectionCount() == 1)
 			{
 				menu.addSeparator();
-				this.addMenuItems(menu, ['editLink'], null, evt);
+				this.addMenuItems(menu, ['editMetadata', 'editLink'], null, evt);
 
 				// Shows edit image action if there is an image in the style
 				if (graph.getModel().isVertex(cell) && mxUtils.getValue(state.style, mxConstants.STYLE_IMAGE, null) != null)
