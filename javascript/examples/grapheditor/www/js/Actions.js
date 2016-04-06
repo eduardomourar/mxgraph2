8d91ee2b193804b335671f1dce942ea5c8305037
Gaudenz Alder <alderg@gmail.com>
2016-04-06 08:19:27 +0200

    Enables edit dialog for read-only files

--- javascript/examples/grapheditor/www/js/Actions.js
+++ javascript/examples/grapheditor/www/js/Actions.js
@@ -73,7 +73,7 @@ Actions.prototype.init = function()
 		var dlg = new EditDiagramDialog(ui);
 		ui.showDialog(dlg.container, 620, 420, true, true);
 		dlg.init();
-	}).isEnabled = isGraphEnabled;
+	});
 	this.addAction('pageSetup...', function() { ui.showDialog(new PageSetupDialog(ui).container, 320, 220, true, true); }).isEnabled = isGraphEnabled;
 	this.addAction('print...', function() { ui.showDialog(new PrintDialog(ui).container, 300, 180, true, true); }, null, 'sprite-print', 'Ctrl+P');
 	this.addAction('preview', function() { mxUtils.show(graph, null, 10, 10); });
