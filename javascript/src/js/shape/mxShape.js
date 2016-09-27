970420c275a210c4dcb61ec4a40e78c0bfe0da7e
Gaudenz Alder <alderg@gmail.com>
2016-09-27 08:31:05 +0200

    Fixes possible NPEs

--- javascript/src/js/shape/mxShape.js
+++ javascript/src/js/shape/mxShape.js
@@ -540,7 +540,7 @@ mxShape.prototype.createCanvas = function()
 		canvas = this.createVmlCanvas();
 	}
 	
-	if (this.outline)
+	if (canvas != null && this.outline)
 	{
 		canvas.setStrokeWidth(this.strokewidth);
 		canvas.setStrokeColor(this.stroke);
