800eb957ba0a9e529e5152f9ca41fe92f464eba9
Gaudenz Alder <gaudenz@jgraph.com>
2017-12-20 16:22:23 +0100

    Fixes redraw after remove change in mxEdgeHandler

--- javascript/src/js/handler/mxEdgeHandler.js
+++ javascript/src/js/handler/mxEdgeHandler.js
@@ -37,7 +37,13 @@ function mxEdgeHandler(state)
 		// Handles escape keystrokes
 		this.escapeHandler = mxUtils.bind(this, function(sender, evt)
 		{
+			var dirty = this.index != null;
 			this.reset();
+			
+			if (dirty)
+			{
+				this.graph.cellRenderer.redraw(this.state, false, state.view.isRendering());
+			}
 		});
 		
 		this.state.view.graph.addListener(mxEvent.ESCAPE, this.escapeHandler);
