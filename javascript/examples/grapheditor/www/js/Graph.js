6b58655a32f5e1f8c9d2598df006de8622b8539f
Ashraf Teleb <ashraf.teleb85@gmail.com>
2018-06-30 16:19:37 +0200

    Native Zoom: Fixing page brakes

--- javascript/examples/grapheditor/www/js/Graph.js
+++ javascript/examples/grapheditor/www/js/Graph.js
@@ -1332,6 +1332,30 @@ Graph.prototype.init = function(container)
 		}
 	};
 
+	var graphUpdatePageBreaks = mxGraph.prototype.updatePageBreaks;
+	
+	mxGraph.prototype.updatePageBreaks = function(visible, width, height)
+	{
+		var useCssTranforms = this.useCssTransforms, scale = this.view.scale, 
+			translate = this.view.translate;
+	
+		if (useCssTranforms)
+		{
+			this.view.scale = 1;
+			this.view.translate = new mxPoint(0, 0);
+			this.useCssTransforms = false;
+		}
+		
+		graphUpdatePageBreaks.apply(this, arguments);
+		
+		if (useCssTranforms)
+		{
+			this.view.scale = scale;
+			this.view.translate = translate;
+			this.useCssTransforms = true;
+		}
+	};
+	
 })();
 
 /**
