0077cb05bfdfa7e309230eeaaad1ff30e7aac2ed
Gaudenz Alder <gaudenz@jgraph.com>
2020-05-28 12:40:21 +0200

    Uses shift for title, ctrl/cmd for crossfunctional swimlanes

--- javascript/examples/grapheditor/www/js/Toolbar.js
+++ javascript/examples/grapheditor/www/js/Toolbar.js
@@ -192,9 +192,10 @@ Toolbar.prototype.addTableDropDown = function()
 		{
 			this.editorUi.menus.addInsertTableItem(menu, mxUtils.bind(this, function(evt, rows, cols)
 			{
-				var table = (mxEvent.isShiftDown(evt)) ? graph.createCrossFunctionalSwimlane(rows, cols) :
-					graph.createTable(rows, cols, null, null, (mxEvent.isControlDown(evt) ||
-					mxEvent.isMetaDown(evt)) ? 'Table' : null);
+				var table = (mxEvent.isControlDown(evt) || mxEvent.isMetaDown(evt)) ?
+					graph.createCrossFunctionalSwimlane(rows, cols) :
+					graph.createTable(rows, cols, null, null,
+					(mxEvent.isShiftDown(evt)) ? 'Table' : null);
 				var pt = (mxEvent.isAltDown(evt)) ? graph.getFreeInsertPoint() :
 					graph.getCenterInsertPoint(graph.getBoundingBoxFromGeometry([table], true));
 				var select = graph.importCells([table], pt.x, pt.y);
