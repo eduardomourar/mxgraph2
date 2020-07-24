ec04af683e7e69873cb50e28ce2a508dff8f6fca
Gaudenz Alder <gaudenz@jgraph.com>
2020-07-24 07:43:09 +0200

    Fixes zoom for fixed size hexagon perimeter

--- javascript/examples/grapheditor/www/js/Shapes.js
+++ javascript/examples/grapheditor/www/js/Shapes.js
@@ -2015,6 +2015,11 @@
 			size = mxUtils.getValue(vertex.style, 'size', size);
 		}
 		
+		if (fixed)
+		{
+			size *= vertex.view.scale;
+		}
+		
 		var x = bounds.x;
 		var y = bounds.y;
 		var w = bounds.width;
