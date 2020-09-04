23c1bc80264b4c89ea3b8c98c598a18cff623930
Gaudenz Alder <gaudenz@jgraph.com>
2020-09-04 15:45:47 +0200

    Syntax

--- javascript/examples/grapheditor/www/js/Graph.js
+++ javascript/examples/grapheditor/www/js/Graph.js
@@ -5,12 +5,12 @@
 // see https://code.google.com/p/google-caja/issues/detail?can=2&q=&colspec=ID%20Type%20Status%20Priority%20Owner%20Summary&groupby=&sort=&id=1296
 if (typeof html4 !== 'undefined')
 {
-	html4.ATTRIBS["a::target"] = 0;
-	html4.ATTRIBS["source::src"] = 0;
-	html4.ATTRIBS["video::src"] = 0;
+	html4.ATTRIBS['a::target'] = 0;
+	html4.ATTRIBS['source::src'] = 0;
+	html4.ATTRIBS['video::src'] = 0;
 	// Would be nice for tooltips but probably a security risk...
-	//html4.ATTRIBS["video::autoplay"] = 0;
-	//html4.ATTRIBS["video::autobuffer"] = 0;
+	//html4.ATTRIBS['video::autoplay'] = 0;
+	//html4.ATTRIBS['video::autobuffer'] = 0;
 }
 
 // Workaround for handling named HTML entities in mxUtils.parseXml
